460ca9a63d854109ab982dcec1f7b6ed05d64a08
willianAD <willian.afonso013@gmail.com>
2023-06-02 17:47:30 -0300

    feat: challenge_palindromes_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,8 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+    word = word.lower()
+    word = word.replace(" ", "")
+    invertida = word[::-1]
+
+    return word == invertida
