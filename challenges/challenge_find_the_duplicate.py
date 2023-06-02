134f4324de6081902325c12bd5631c9073f9f21c
willianAD <willian.afonso013@gmail.com>
2023-06-02 20:59:40 -0300

    coment challenge_find_the_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,16 +1,16 @@
-def find_duplicate(nums):
-    if not nums or isinstance(nums, str):
-        return False
-    if len(nums) < 2:
-        return False
-    if any(num < 0 for num in nums):
-        return False
+# def find_duplicate(nums):
+#     if not nums or isinstance(nums, str):
+#         return False
+#     if len(nums) < 2:
+#         return False
+#     if any(num < 0 for num in nums):
+#         return False
 
-    nums_set = set()
-    for num in nums:
-        if num in nums_set:
-            return num
-        else:
-            nums_set.add(num)
+#     nums_set = set()
+#     for num in nums:
+#         if num in nums_set:
+#             return num
+#         else:
+#             nums_set.add(num)
 
-    return False
+#     return False
