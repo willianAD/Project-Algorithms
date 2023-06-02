4b418d8e6828ac269d0012247d6ecaad2beb9394
willianAD <willian.afonso013@gmail.com>
2023-06-02 18:22:58 -0300

    feat: challenge_palindromes_recursive - Requisito 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
     if len(word) == 0:
         return False
+    word = word.lower()
+    word = word.replace(" ", "")
+    if high_index <= 1:
+        return True
+    elif word[low_index] != word[high_index]:
+        return False
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
