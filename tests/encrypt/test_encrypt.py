aa2a8064208caad792dddb0593a273416522a961
willianAD <willian.afonso013@gmail.com>
2023-06-02 16:58:24 -0300

    test: test_encrypt - Requisito 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("Hello Word!", 3) == "leH_!droW ol"
+    assert encrypt_message("WillianAfonso", 7) == "nailliW_osnofA"
+    assert encrypt_message("Dicas Pythonicas", 6) == "sacinohtyP_ saciD"
+    assert encrypt_message("SemUnder", 10) == "rednUmeS"
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("ivalid", "key")
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(13, 9)
