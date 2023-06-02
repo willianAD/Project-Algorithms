801aa4c8b0060e2936f1f6eb80b42f7fb5e98b9c
willianAD <willian.afonso013@gmail.com>
2023-06-02 20:55:46 -0300

    feat: refactoring challenge_study_schedule

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,17 +1,16 @@
-# def find_duplicate(nums):
-#     if nums is None:
-#         return False
-#     if isinstance(nums, str):
-#         return False
-#     if not isinstance(nums, list) or len(nums) <= 1:
-#         return False
-#     if any(num < 0 for num in nums):
-#         return False
+def find_duplicate(nums):
+    if not nums or isinstance(nums, str):
+        return False
+    if len(nums) < 2:
+        return False
+    if any(num < 0 for num in nums):
+        return False
 
-#     nums_set = set()
-#     for num in nums:
-#         if num in nums_set:
-#             return num
-#         nums_set.add(num)
+    nums_set = set()
+    for num in nums:
+        if num in nums_set:
+            return num
+        else:
+            nums_set.add(num)
 
-#     return False
+    return False
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,11 +1,10 @@
 def study_schedule(permanence_period, target_time):
+    if not isinstance(target_time, int):
+        return None
+
     student = 0
     for period in permanence_period:
-        if (
-            type(target_time) != int
-            or type(period[0]) != int
-            or type(period[1]) != int
-        ):
+        if not (isinstance(period[0], int) and isinstance(period[1], int)):
             return None
         if period[0] <= target_time <= period[1]:
             student += 1
