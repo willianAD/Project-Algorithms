577ea7f8311e7c6e1021e0148c26f69ee74ca0c6
willianAD <willian.afonso013@gmail.com>
2023-06-02 16:04:38 -0300

    feat: challenge_study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    student = 0
+    for period in permanence_period:
+        if (
+            type(target_time) != int
+            or type(period[0]) != int
+            or type(period[1]) != int
+        ):
+            return None
+        if period[0] <= target_time <= period[1]:
+            student += 1
+    return student
