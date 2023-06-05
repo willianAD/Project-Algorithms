a4700e6302f2e7cfac3d3432fe3e2b0ae6da6629
willianAD <willian.afonso013@gmail.com>
2023-06-05 14:37:38 -0300

    feat: challenge_find_the_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,16 +1,14 @@
-# def find_duplicate(nums):
-#     if not nums or isinstance(nums, str):
-#         return False
-#     if len(nums) < 2:
-#         return False
-#     if any(num < 0 for num in nums):
-#         return False
+def find_duplicate(nums):
+    if not nums or isinstance(nums, str) or len(nums) < 2:
+        return False
 
-#     nums_set = set()
-#     for num in nums:
-#         if num in nums_set:
-#             return num
-#         else:
-#             nums_set.add(num)
+    nums_set = set()
+    for num in nums:
+        if type(num) == str or num < 0:
+            return False
+        elif num in nums_set:
+            return num
+        else:
+            nums_set.add(num)
 
-#     return False
+    return False
